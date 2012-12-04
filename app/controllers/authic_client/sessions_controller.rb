86d41d4ba43c8da893e306acd4989146653694b7
Carl <ctrand@gmail.com>
2012-12-04 19:45:51 +1100

    Cleared session return to url after use

--- app/controllers/authic_client/sessions_controller.rb
+++ app/controllers/authic_client/sessions_controller.rb
@@ -3,7 +3,9 @@ class AuthicClient::SessionsController < ApplicationController
     user = User.find_for_authic_oauth(request.env["omniauth.auth"], current_user)
     if user.persisted?
       session[:authic_user_id] = user.id
-      redirect_path ||= session.delete(:authic_return_to_this_url) ||= root_url
+      redirect_path ||= session[:authic_return_to_this_url] ||= root_url
+      # make sure the return to session variable is nil so it cant accidently get re-used
+      session[:authic_return_to_this_url] = nil 
       redirect_to redirect_path, :notice => "You have been successfully signed in"
     else
       redirect_to root_url  
