168a0c9cc3d282f9276ae513a39369970cf3c9bf
Carl <ctrand@gmail.com>
2012-12-04 19:32:22 +1100

    Better session cleanup around signin and signout

--- app/controllers/authic_client/sessions_controller.rb
+++ app/controllers/authic_client/sessions_controller.rb
@@ -3,7 +3,7 @@ class AuthicClient::SessionsController < ApplicationController
     user = User.find_for_authic_oauth(request.env["omniauth.auth"], current_user)
     if user.persisted?
       session[:authic_user_id] = user.id
-      redirect_path ||= session[:authic_return_to_this_url] ||= root_url
+      redirect_path ||= session.delete(:authic_return_to_this_url) ||= root_url
       redirect_to redirect_path, :notice => "You have been successfully signed in"
     else
       redirect_to root_url  
@@ -11,7 +11,7 @@ class AuthicClient::SessionsController < ApplicationController
   end
 
   def destroy
-    session[:authic_user_id] = nil
+    reset_session
     redirect_path = "#{AuthicClient::AUTHIC_CLIENT_FULL_URL}/authic_sign_out?&return_path=#{URI.escape root_url}"
     redirect_to redirect_path
   end
