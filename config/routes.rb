a51ac7f88f8301b3b5c6d5cec7d9c4cb7e7fa2bc
James P. McGrath <james@reinteractive.net>
2014-08-16 23:15:07 +1000

    Use delete verb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get   'auth/authic/callback' => 'authic/sessions#create'
-  match 'signout' => 'authic/sessions#destroy', as: 'signout', via: [:get, :post]
+  match 'signout' => 'authic/sessions#destroy', as: 'signout', via: [:get, :delete]
 end
