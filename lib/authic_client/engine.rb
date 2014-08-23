ff85c06c5f6e649c55aa0a8a42e045062a69cf88
James P. McGrath <james@reinteractive.net>
2014-08-24 08:31:18 +1000

    load the helpers in a different way

--- lib/authic_client/engine.rb
+++ lib/authic_client/engine.rb
@@ -2,10 +2,8 @@ module AuthicClient
   class Engine < ::Rails::Engine
     isolate_namespace AuthicClient
 
-    initializer 'authic_client.app_controller' do |app|
-      ActiveSupport.on_load(:action_controller) do
-        include AuthicClient::ApplicationHelper
-      end
+    config.to_prepare do
+      ApplicationController.helper(AuthicClient::ApplicationHelper)
     end
   end
 end
