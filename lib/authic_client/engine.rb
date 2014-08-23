a69d3ad46d49e1136e63fe009863d5041bf83b26
James P. McGrath <james@reinteractive.net>
2014-08-23 22:46:32 +1000

    include the helpers into the app

--- lib/authic_client/engine.rb
+++ lib/authic_client/engine.rb
@@ -1,5 +1,11 @@
 module AuthicClient
   class Engine < ::Rails::Engine
     isolate_namespace AuthicClient
+
+    initializer 'authic_client.app_controller' do |app|
+      ActiveSupport.on_load(:action_controller) do
+        include AuthicClient::ApplicationHelper
+      end
+    end
   end
 end
