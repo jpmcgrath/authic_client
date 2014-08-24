926d77e36ee9e8ab33683e52457e04faf8eadca8
James P. McGrath <james@reinteractive.net>
2014-08-24 21:03:30 +1000

    turn off isolation

--- lib/authic_client/engine.rb
+++ lib/authic_client/engine.rb
@@ -1,11 +1,11 @@
 module AuthicClient
   class Engine < ::Rails::Engine
-    isolate_namespace AuthicClient
+    # isolate_namespace AuthicClient
 
-    initializer 'authic_client.app_controller' do |app|
-      ActiveSupport.on_load(:action_controller) do
-        ActionController::Base.helper AuthicClient::ApplicationHelper
-      end
-    end
+    # initializer 'authic_client.app_controller' do |app|
+    #   ActiveSupport.on_load(:action_controller) do
+    #     ActionController::Base.helper AuthicClient::ApplicationHelper
+    #   end
+    # end
   end
 end
