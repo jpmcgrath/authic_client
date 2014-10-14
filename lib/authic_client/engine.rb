ea4e7328a87de8660a09a9a7c8eed99be22bfa49
James P. McGrath <james@reinteractive.net>
2014-10-14 21:20:14 +1000

    fix for generators

--- lib/authic_client/engine.rb
+++ lib/authic_client/engine.rb
@@ -4,6 +4,11 @@ require 'omniauth-authic'
 module AuthicClient
   class Engine < ::Rails::Engine
     isolate_namespace AuthicClient
+    initializer 'authic_client.app_controller' do |app|
+      ActiveSupport.on_load(:action_controller) do
+        include AuthicClient::ApplicationHelper
+      end
+    end
 
     config.generators do |g|
       g.test_framework :rspec, :fixture => false
