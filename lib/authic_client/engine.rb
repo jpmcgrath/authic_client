7379fe2e839b2f11e5b9e8634c5cfa8a2287ffd0
James P. McGrath <james@reinteractive.net>
2014-10-14 21:42:04 +1000

    do not isolate namespace

--- lib/authic_client/engine.rb
+++ lib/authic_client/engine.rb
@@ -3,7 +3,7 @@ require 'omniauth-authic'
 
 module AuthicClient
   class Engine < ::Rails::Engine
-    isolate_namespace AuthicClient
+    #isolate_namespace AuthicClient
     initializer 'authic_client.app_controller' do |app|
       ActiveSupport.on_load(:action_controller) do
         include AuthicClient::ApplicationHelper
